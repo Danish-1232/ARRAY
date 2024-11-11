12ce80fa437f8922d812f32f8d6088922422a758
Danish-Farooqui <danishfarooqui303@gmail.com>
2024-11-11 15:21:41 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,27 @@
+// No -1
+
+var fruits = ["Banana","Mango","Guava","Apple"];
+console.log(fruits);
+
+// No -2
+
+
+var countries = ["Saudia","Palestine","Turkey"];
+console.log(countries);
+
+// No -3
+
+var movies = ["Aladin","Mission Impossible","12th Fail"];
+console.log(movies);
+
+// No -4
+
+var days = ["Sunday","Monday","Tuesday","Wednusday","Thursday","Friday","Saturday"];
+console.log(days);
+
+// No -5
+
+var color = ["Black","Blue","Gray","White"];
+console.log(color);
+
+
